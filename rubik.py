930dc559669ecf8f4792d0fb6be56010e988a8c7
H4CKY54CK <tk13xr37@gmail.com>
2020-04-27 05:47:46 -0400

    Update rubik.py

--- rubik.py
+++ rubik.py
@@ -7,27 +7,16 @@ from itertools import product, count
 from collections import deque
 import numpy as np
 
-class Map:
+class Cube:
     def __init__(self):
         
-        self.grid = np.array([0 for _ in range(108)]).reshape(9,12)
-        for x, y in product(range(3,6), range(0,3)):
-            self.grid[x,y] = 1
-        for x, y in product(range(3,6), range(3,6)):
-            self.grid[x,y] = 2
-        for x, y in product(range(0,3), range(6,9)):
-            self.grid[x,y] = 5
-        for x, y in product(range(3,6), range(9,12)):
-            self.grid[x,y] = 4
-        for x, y in product(range(6,9), range(6,9)):
-            self.grid[x,y] = 6
-        for x, y in product(range(3,6), range(6,9)):
-            self.grid[x,y] = 3
-
-        N = iter(range(9))
-
-        # for x, y in product(range(0,3), range(6,9)):
-            # self.grid[x,y] = next(N)
+        self.grid = np.zeros((9,12), dtype=int)
+        self.grid[3:6,0:3] = 1
+        self.grid[3:6,3:6] = 2
+        self.grid[3:6,6:9] = 3
+        self.grid[3:6,9:12] = 4
+        self.grid[0:3,6:9] = 5
+        self.grid[6:9,6:9] = 6
 
     def __str__(self):
 
@@ -132,10 +121,10 @@ class Map:
 
         if counter:
             self.grid[np.ix_([3,4,5],[0,1,2])] = np.rot90(self.grid[np.ix_([3,4,5],[0,1,2])], -1)
-            self.grid[0,6:9], self.grid[3:6,11], self.grid[8,6:9], self.grid[3:6,3] = self.grid[3:6,11].copy(), self.grid[8,6:9].copy(), self.grid[3:6,3].copy(), self.grid[0,6:9].copy()
+            self.grid[0,6:9], self.grid[3:6,11], self.grid[8,6:9], self.grid[3:6,3] = self.grid[3:6,11], self.grid[8,6:9], self.grid[3:6,3], self.grid[0,6:9].copy()
         else:
             self.grid[np.ix_([3,4,5],[0,1,2])] = np.rot90(self.grid[np.ix_([3,4,5],[0,1,2])])
-            self.grid[0,6:9], self.grid[3:6,11], self.grid[8,6:9], self.grid[3:6,3] = self.grid[3:6,11].copy(), self.grid[8,6:9].copy(), self.grid[3:6,3].copy(), self.grid[0,6:9].copy()
+            self.grid[0,6:9], self.grid[3:6,11], self.grid[8,6:9], self.grid[3:6,3] = self.grid[3:6,11], self.grid[8,6:9], self.grid[3:6,3], self.grid[0,6:9].copy()
     def move(self, alg):
 
         moves = alg.split()
@@ -200,7 +189,7 @@ class Map:
     #             avg = sum(right)/len(right)
     #             print(f"RIGHT {avg:.02f} nanoseconds")
 
-m = Map()
+m = Cube()
 while True:
     os.system('cls')
     print(m)
